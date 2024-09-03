2a14ae13fbafe82224f9898a0cfc1a1bcdcd216e
Andreas Palmqvist <andpa_kontakt@live.se>
2024-09-03 14:53:09 +0200

    Refactor touch control handling

--- app/src/main/java/se/umu/ad/anpa0292/voxelsculpter/EditorSurfaceView.kt
+++ app/src/main/java/se/umu/ad/anpa0292/voxelsculpter/EditorSurfaceView.kt
@@ -15,15 +15,6 @@ class EditorSurfaceView(context: Context, attributeSet: AttributeSet) : GLSurfac
 
     private var prevDistance = 0f
 
-    private enum class TouchMode {
-        IDLE,
-        SINGLE_TAP,
-        SINGLE_DRAG,
-        TWO_POINTER_TOUCH
-    }
-
-    private var touchMode = TouchMode.IDLE
-
     companion object {
         const val ROTATION_SENSITIVITY = 0.3f
         const val ZOOM_SENSITIVITY = 0.1f
@@ -48,67 +39,55 @@ class EditorSurfaceView(context: Context, attributeSet: AttributeSet) : GLSurfac
         event ?: return false
 
         when (event.actionMasked) {
-            MotionEvent.ACTION_DOWN -> {
-                prevX = event.x
-                prevY = event.y
-
-                touchMode = if (event.pointerCount == 1) TouchMode.SINGLE_TAP else TouchMode.IDLE
-            }
-
-            MotionEvent.ACTION_POINTER_DOWN ->
-                touchMode =
-                    if (event.pointerCount == 2) {
-                        val x1 = event.getX(0)
-                        val y1 = event.getY(0)
-                        val x2 = event.getX(1)
-                        val y2 = event.getY(1)
-                        prevDistance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
-
-                        TouchMode.TWO_POINTER_TOUCH
-                    }
-                    else
-                        TouchMode.IDLE
-
-            MotionEvent.ACTION_MOVE -> {
-                when (touchMode) {
-                    TouchMode.IDLE -> {} // Do nothing when more than 2 pointers are used
-
-                    TouchMode.SINGLE_TAP -> touchMode = TouchMode.SINGLE_DRAG
-
-                    TouchMode.SINGLE_DRAG -> {
-                        val dx = event.x - prevX
-                        val dy = event.y - prevY
-                        renderer.camera.rotate(
-                            -dx * ROTATION_SENSITIVITY,
-                            dy * ROTATION_SENSITIVITY
-                        )
-
-                        prevX = event.x
-                        prevY = event.y
-                    }
-
-                    TouchMode.TWO_POINTER_TOUCH -> {
-                        if (event.pointerCount == 1) {
-                            prevX = event.x
-                            prevY = event.y
-                            touchMode = TouchMode.SINGLE_DRAG
-                        }
-                        else if (event.pointerCount == 2) {
-                            val x1 = event.getX(0)
-                            val y1 = event.getY(0)
-                            val x2 = event.getX(1)
-                            val y2 = event.getY(1)
-                            val distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
-
-                            val zoomDelta = prevDistance - distance
-                            renderer.camera.zoom(zoomDelta * ZOOM_SENSITIVITY)
-                            prevDistance = distance
-                        }
-                    }
-                }
-            }
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_POINTER_DOWN -> handleDown(event)
+            MotionEvent.ACTION_MOVE -> handleMove(event)
         }
 
         return true
+    }
+
+    private fun handleDown(event: MotionEvent) {
+        when (event.pointerCount) {
+            1 -> {
+                prevX = event.x
+                prevY = event.y
+            }
+            2 -> {
+                prevDistance = calculateDistance(event)
+            }
+        }
+    }
+
+    private fun handleMove(event: MotionEvent) {
+        when (event.pointerCount) {
+            1 -> handleSingleDrag(event)
+            2 -> handlePinchZoom(event)
+        }
+    }
+
+    private fun handleSingleDrag(event: MotionEvent) {
+        val dx = event.x - prevX
+        val dy = event.y - prevY
+        renderer.camera.rotate(
+            -dx * ROTATION_SENSITIVITY,
+            dy * ROTATION_SENSITIVITY
+        )
+        prevX = event.x
+        prevY = event.y
+    }
+
+    private fun handlePinchZoom(event: MotionEvent) {
+        val distance = calculateDistance(event)
+        val zoomDelta = prevDistance - distance
+        renderer.camera.zoom(zoomDelta * ZOOM_SENSITIVITY)
+        prevDistance = distance
+    }
+
+    private fun calculateDistance(event: MotionEvent): Float {
+        val x1 = event.getX(0)
+        val y1 = event.getY(0)
+        val x2 = event.getX(1)
+        val y2 = event.getY(1)
+        return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
     }
 }
